c3bb5fe0ce2ce96349aaa2e4946b10832fa197ba
VuzLe <tle63917@gmail.com>
2025-08-28 11:30:55 +0700

    thêm data

---
+++ MyWeb/Data/CartStorage.cs
@@ -0,0 +1,28 @@
+﻿using MyWeb.Models;
+
+namespace MyWeb.Data
+{
+    public class CartStorage
+    {
+        private static List<Product> cartList = new List<Product>();
+
+        public static List<Product> GetCart()
+        {
+            return cartList;
+        }
+
+        public static void AddToCart(Product product)
+        {
+            cartList.Add(product);
+        }
+
+        public static void RemoveFromCart(int id)
+        {
+            var product = cartList.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                cartList.Remove(product);
+            }
+        }
+    }
+}
