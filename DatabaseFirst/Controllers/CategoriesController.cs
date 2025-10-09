d3bd35680e9af3749073c8ea0c5968db20967cbc
VuzLe <tle63917@gmail.com>
2025-10-09 23:10:48 +0700

    feat: add CRUD db for category

---
+++ DatabaseFirst/Controllers/CategoriesController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DatabaseFirst.Models;
+using System.Linq;
+
+namespace DatabaseFirst.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly BookStoreContext _context;
+
+        public CategoriesController(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            if (string.IsNullOrEmpty(HttpContext.Session.GetString("Username")))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            return View(_context.Categories.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Add(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var cat = _context.Categories.Find(id);
+            if (cat == null) return NotFound();
+            return View(cat);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Category category)
+        {
+            if (id != category.CategoryId) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(category);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var cat = _context.Categories.Find(id);
+            if (cat == null) return NotFound();
+
+            _context.Categories.Remove(cat);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
