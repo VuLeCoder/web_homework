50bea497c19620be05495db15790291f0ff951f5
VuzLe <tle63917@gmail.com>
2025-10-12 21:11:56 +0700

    feat: add CRUD SanPham

---
+++ CodeFirst/CodeFirst/Controllers/SanPhamsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CodeFirst.Models.DBModels;
+using CodeFirst.Models.DataModels;
+
+namespace CodeFirst.Controllers
+{
+    public class SanPhamsController : Controller
+    {
+        private readonly ShoppingCartContext _context;
+
+        public SanPhamsController(ShoppingCartContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SanPhams
+        public async Task<IActionResult> Index()
+        {
+            var shoppingCartContext = _context.SanPhams.Include(s => s.LoaiSanPham);
+            return View(await shoppingCartContext.ToListAsync());
+        }
+
+        // GET: SanPhams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams
+                .Include(s => s.LoaiSanPham)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Create
+        public IActionResult Create()
+        {
+            ViewData["LoaiSanPhamId"] = new SelectList(_context.LoaiSanPhams, "Id", "TenLoai");
+            return View();
+        }
+
+        // POST: SanPhams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,MaSanPham,TenSanPham,HinhAnh,SoLuong,DonGia,TrangThai,LoaiSanPhamId")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sanPham);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LoaiSanPhamId"] = new SelectList(_context.LoaiSanPhams, "Id", "TenLoai", sanPham.LoaiSanPhamId);
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams.FindAsync(id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+            ViewData["LoaiSanPhamId"] = new SelectList(_context.LoaiSanPhams, "Id", "TenLoai", sanPham.LoaiSanPhamId);
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,MaSanPham,TenSanPham,HinhAnh,SoLuong,DonGia,TrangThai,LoaiSanPhamId")] SanPham sanPham)
+        {
+            if (id != sanPham.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sanPham);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SanPhamExists(sanPham.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["LoaiSanPhamId"] = new SelectList(_context.LoaiSanPhams, "Id", "TenLoai", sanPham.LoaiSanPhamId);
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams
+                .Include(s => s.LoaiSanPham)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sanPham = await _context.SanPhams.FindAsync(id);
+            if (sanPham != null)
+            {
+                _context.SanPhams.Remove(sanPham);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SanPhamExists(int id)
+        {
+            return _context.SanPhams.Any(e => e.Id == id);
+        }
+    }
+}
