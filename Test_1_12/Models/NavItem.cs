714cf0ad1c078b5ab201fdf0920aa362dfa38c5c
VuzLe <tle63917@gmail.com>
2025-12-01 12:33:38 +0700

    feat: add 2.1

---
+++ Test_1_12/Models/NavItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test_1_12.Models;
+
+public partial class NavItem
+{
+    public int Id { get; set; }
+
+    public string NavName { get; set; } = null!;
+
+    public string? NavNameVn { get; set; }
+}
