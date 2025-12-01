c7a9c59b04acce0beda325f2e99501583e98af7b
VuzLe <tle63917@gmail.com>
2025-12-01 12:44:20 +0700

    feat: add 2.1

--- Test_1_12/Program.cs
+++ Test_1_12/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Test_1_12.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ShopDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
