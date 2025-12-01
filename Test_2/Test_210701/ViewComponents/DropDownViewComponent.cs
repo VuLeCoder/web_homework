df86ce9fae29c1600d60698a1a998bd0d711f34c
VuzLe <tle63917@gmail.com>
2025-12-01 19:22:23 +0700

    feat: add 2.2

---
+++ Test_2/Test_210701/ViewComponents/DropDownViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_210701.Models;
+
+namespace Test_210701.ViewComponents
+{
+    public class DropDownViewComponent : ViewComponent
+    {
+        private readonly ShopDbContext _shopDbContext;
+
+        public DropDownViewComponent(ShopDbContext shopDbContext)
+        {
+            _shopDbContext = shopDbContext;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _shopDbContext.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
