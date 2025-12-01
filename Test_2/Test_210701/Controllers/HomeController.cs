64c72cf2dbd2a10eb0538ac586856196eec93a59
VuzLe <tle63917@gmail.com>
2025-12-01 19:27:01 +0700

    feat: add 3

--- Test_2/Test_210701/Controllers/HomeController.cs
+++ Test_2/Test_210701/Controllers/HomeController.cs
@@ -7,15 +7,20 @@ namespace Test_210701.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ShopDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ShopDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
-            return View("Vuz_MainContent");
+            var query = _context.Products.Where(p => p.Available == true);
+
+            var productList = query.ToList();
+            return View("Vuz_MainContent", productList);
         }
 
         public IActionResult Privacy()
