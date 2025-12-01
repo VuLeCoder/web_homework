ab28a36fadef32847aba13e25d072e2142de0884
VuzLe <tle63917@gmail.com>
2025-12-01 12:45:17 +0700

    feat: add 2.2

---
+++ Test_1_12/ViewComponents/leftCategoriesViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test_1_12.Models;
+
+namespace Test_1_12.ViewComponents
+{
+    public class leftCategoriesViewComponent : ViewComponent
+    {
+        public readonly ShopDbContext _context;
+
+        public leftCategoriesViewComponent(ShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var categories = _context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
