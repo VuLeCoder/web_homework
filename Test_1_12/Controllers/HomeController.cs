86ee0ed15e1c823126948d3896324e194d586a12
VuzLe <tle63917@gmail.com>
2025-12-01 12:29:44 +0700

    feat: add 1.1

--- Test_1_12/Controllers/HomeController.cs
+++ Test_1_12/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Test_1_12.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View("Vuz_MainContent");
         }
 
         public IActionResult Privacy()
