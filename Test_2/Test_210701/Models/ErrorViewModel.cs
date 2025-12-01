5d9a56fadd6ce5ce0c93150ad646f33d80d25d4b
VuzLe <tle63917@gmail.com>
2025-12-01 15:20:08 +0700

    feat: init project-test2

---
+++ Test_2/Test_210701/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Test_210701.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
